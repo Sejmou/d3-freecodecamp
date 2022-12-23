1512bc2177aa44fe54d0bd959d93a4aeb76561d1
Sejmou <samo.kolter@gmail.com>
2022-12-23 18:28:07 +0100

    draw horizontal lines across screen

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,25 @@ const svg = select('body')
   .append('svg')
   .attr('width', width)
   .attr('height', height);
+
+const n = 100;
+
+const marks = range(n).map(i => ({
+  y: i * 20,
+  width: width,
+  height: 10,
+  // mask: 'url(#circle-mask)',// will be relevant later
+}));
+
+svg
+  .selectAll('rect')
+  .data(marks) // produces d3 data join
+  .join('rect') // simplified API compared to enter/exit etc. - will create one rect for each mark
+  .attr('y', d => d.y) // assign attributes to each rect
+  .attr('width', d => d.width)
+  .attr('height', d => d.height);
+//.attr('mask', d => d.mask);// will be relevant later
+
+function range(number: number) {
+  return Array.from(Array(number).keys());
+}
