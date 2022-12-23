a8d3641020e5173a271947f5620d5ef095ce390f
Sejmou <samo.kolter@gmail.com>
2022-12-23 18:59:30 +0100

    use mask to limit horizontal lines to circle in center of screen

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,33 @@ const svg = select('body')
   .attr('width', width)
   .attr('height', height);
 
+// other SVG elements can refer to "masks" - masks can be of arbitrary shape and can be used to hide or show content
+// in this example, we create a circle mask and will use it to show vertical lines "inside" of it (where it is white) and hide them outside of it (where it is black)
+const circleMask = svg.append('mask').attr('id', 'circle-mask');
+circleMask
+  .append('circle')
+  .attr('cx', width / 2)
+  .attr('cy', height / 2)
+  .attr('r', '25%')
+  .attr('fill', 'white'); // if a mask is filled white, it will show the content, if it is filled black, it will hide the content
+
 const n = 100;
 
-const marks = range(n).map(i => ({
+const horizMarks = range(n).map(i => ({
   y: i * 20,
   width: width,
   height: 10,
-  // mask: 'url(#circle-mask)',// will be relevant later
+  mask: 'url(#circle-mask)',
 }));
 
 svg
   .selectAll('rect')
-  .data(marks) // produces d3 data join
+  .data(horizMarks) // produces d3 data join
   .join('rect') // simplified API compared to enter/exit etc. - will create one rect for each mark
   .attr('y', d => d.y) // assign attributes to each rect
   .attr('width', d => d.width)
-  .attr('height', d => d.height);
-//.attr('mask', d => d.mask);// will be relevant later
+  .attr('height', d => d.height)
+  .attr('mask', d => d.mask);
 
 function range(number: number) {
   return Array.from(Array(number).keys());
