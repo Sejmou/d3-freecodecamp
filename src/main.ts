8fc115bc0feff6618852cd587a383682b7734e29
Sejmou <samo.kolter@gmail.com>
2022-12-24 23:32:25 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css';
+import { select } from 'd3';
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const svg = select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
