88e4ac4fab4e86d4b475c0bb302f4a58795b2291
Sejmou <samo.kolter@gmail.com>
2022-12-24 23:52:27 +0100

    ascending sort helper demo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
-import { select } from 'd3';
+import { ascending, select } from 'd3';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -8,3 +8,11 @@ const svg = select('body')
   .append('svg')
   .attr('width', width)
   .attr('height', height);
+
+const sortable = [3, 60, 35, 2, 45, 320, 5];
+// Vanilla JS sorting approach:
+// sortable.sort((a, b) => a - b);
+// note: cannot use sort without this sorting function as default sort converts to string and sorts alphabetically
+
+// fancy d3 sorting approach:
+console.log(sortable.sort(ascending));
