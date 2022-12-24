a01c8b1aedcfc1614a0bb8ac723043add2eb6a26
Sejmou <samo.kolter@gmail.com>
2022-12-25 00:33:31 +0100

    enter selection example

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import './style.css';
-import { ascending, select } from 'd3';
+import { select, range } from 'd3';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const svg = select('body')
-  .append('svg')
+const svg = select('body') // selection of all existing DOM elements that match the selector
+  .append('svg') // append returns a new selection
   .attr('width', width)
   .attr('height', height);
 
-const sortable = [
-  { name: 'C', value: 66 },
-  { name: 'A', value: 123 },
-  { name: 'B', value: 2 },
-];
-console.log(sortable.sort((a, b) => ascending(a.value, b.value)));
+const data = range(15);
+
+svg
+  .selectAll('circle')
+  .data(data)
+  .enter() // enter selection contains all data elements that don't have a corresponding DOM element for the provided selector (in this case 'circle)
+  .append('circle')
+  .attr('r', 10)
+  .attr('cx', d => d * 25 + 25)
+  .attr('cy', d => 250 + Math.sin(d * 0.5) * 100);
